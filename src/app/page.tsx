5a1da118d955294816dcd3107ba6f5078f635749
Raj-venom <rajsingh.rsr9824@gmail.com>
2026-01-01 13:43:27 +0545

    feat: integrate Inngest for background task handling
    - Added Inngest CLI for local development.
    - Created Inngest client for event handling.
    - Implemented helloWorld function to process events.
    - Updated API route to serve Inngest functions.
    - Modified home page to invoke background tasks via TRPC.
    - Enhanced app router with a new mutation to trigger Inngest function.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
-import { Client } from "@/components/Client";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client";
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "server" }));
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+export default function Home() {
+
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div>
+      <div className="flex min-h-screen items-center justify-center">
+        <Button onClick={() => invoke.mutate({text: "Raj9824"})} >
+          Invoke Background Task
+        </Button>
+      </div>
+    </div>
   );
 }
