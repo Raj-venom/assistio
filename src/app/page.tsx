ef0af10317ab9b24c761610d7bd939bbf8d6211d
Raj-venom <rajsingh.rsr9824@gmail.com>
2026-01-02 14:58:55 +0545

    feat: integrate Inngest agent for background task invocation
    - Added @inngest/agent-kit dependency to package.json.
    - Updated Home component to include an input field for user text input.
    - Modified helloWorld function to utilize the Inngest agent for generating code snippets based on user input.
    - Adjusted TRPC router to send the correct data structure to the Inngest function.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function Home() {
 
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}))
 
@@ -13,9 +16,16 @@ export default function Home() {
   return (
     <div>
       <div className="flex min-h-screen items-center justify-center">
-        <Button onClick={() => invoke.mutate({text: "Raj9824"})} >
+        <div>
+        <Input
+         placeholder="Enter text to invoke background task" 
+         value={value}
+         onChange={(e) => setValue(e.target.value)}
+         />
+        <Button onClick={() => invoke.mutate({text: value})} >
           Invoke Background Task
         </Button>
+         </div>
       </div>
     </div>
   );
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,26 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("download-the-video", "5s");
 
-    // transcript the video
-    await step.sleep("transcript-the-video", "5s");
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: `You are an expert next.js developer. Write code snippets as per user requirements. If you are asked to write code, ensure that the code is syntactically correct and can run without errors.`,
+      model: gemini({ model: "gemini-2.5-flash" }),
+      
+    });
 
-    // summarize the video
-    await step.sleep("summarize-the-video", "5s");
-   
-    return { message: `Hello ${event.data?.email ?? "Guest"}!` };
-  },
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.text}`
+    );
+
+    return { output };
+
+  }
+
 );
+
