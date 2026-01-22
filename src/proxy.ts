270902924ade4e34f5f7cadc87bba88333f727c6
Raj-venom <rajsingh.rsr9824@gmail.com>
2026-01-22 19:21:41 +0545

    feat: add pricing page and integrate scroll detection in navbar

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/pricing(.*)",
   "/api(.*)", // Allow API routes in proxy Protect it using trpc auth middleware
 ]);
 
